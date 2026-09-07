82b2496e45e06cf6bcf96a365bf71da27a8ac5c7
BetaWing <brafagprettymaiden@gmail.com>
2026-09-07 23:58:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@
     }
     uw.__multbot_index_running__ = true;
 
-    const BASE_URL = 'https://raw.githubusercontent.com/NotXina/MultBot/main/Modules';
+    const BASE_URL = 'https://raw.githubusercontent.com/BetaWing/ruralfixmaybe/main/Modules';
     const MAX_RETRIES = 2;
     const FETCH_TIMEOUT_MS = 15000;
 
